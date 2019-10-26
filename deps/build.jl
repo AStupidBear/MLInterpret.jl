a0fa08771b396791f54cd9b6d4acd5c1faf6b19c
Yao Lu <luyaocns@gmail.com>
2019-10-27 01:37:48 +0800

    dot

--- deps/build.jl
+++ deps/build.jl
@@ -7,7 +7,7 @@ isnothing(Sys.which("sudo")) &&
 run(`apt-get update` & `apt install sudo`)
 install("wget", "wget")
 install("gcc", "build-essential")
-install("graphviz", "graphviz")
+install("dot", "graphviz")
 install("add-apt-repository", "software-properties-common")
 
 run(`$python -m pip install pandas sklearn matplotlib lightgbm shap keras tzlocal`)
