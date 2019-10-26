32b28e8fe6455f5274ae8ba414599a17ca081d29
Yao Lu <luyaocns@gmail.com>
2019-10-27 01:22:47 +0800

    install graphviz

--- deps/build.jl
+++ deps/build.jl
@@ -7,13 +7,12 @@ try run(`apt-get update`) catch end
 install("sudo", "sudo")
 install("wget", "wget")
 install("gcc", "build-essential")
+install("graphviz", "graphviz")
 install("add-apt-repository", "software-properties-common")
 
 run(`$python -m pip install pandas sklearn matplotlib lightgbm shap keras tzlocal`)
 
-Conda.add("graphviz")
 Conda.add("gxx_linux-64")
-
 skater = mktempdir()
 clone("https://github.com/oracle/Skater.git", skater)
 run(`sed -i "s|sudo python|$python|g" $skater/setup.sh`)
