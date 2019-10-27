fc0ba6c0228749ebe4149197018c054856416973
Yao Lu <luyaocns@gmail.com>
2019-10-27 12:40:16 +0800

    update build

--- deps/build.jl
+++ deps/build.jl
@@ -1,14 +1,16 @@
-using PyCall: python, Conda
+using PyCall: python
 using Pkg.GitTools: clone
 
 isnothing(Sys.which("dot")) && run(`sudo apt-get install -y graphviz`)
 
 run(`$python -m pip install pandas sklearn matplotlib lightgbm shap keras tzlocal PyPDF2`)
 
-Conda.add("gxx_linux-64")
-skater = mktempdir()
-clone("https://github.com/oracle/Skater.git", skater)
-run(`sed -i "s|sudo python|$python|g" $skater/setup.sh`)
-run(`sed -i "s|.*install python.*||g" $skater/setup.sh`)
-run(`sed -i "s|apt-get install|apt-get install -y|g" $skater/setup.sh`)
-run(`bash -c "cd $skater && $python setup.py install --ostype=linux-ubuntu --rl=True"`)
+if get(ENV, "FORCE_BUILD_SKATER", "0") == "1" || 
+    !occursin("skater", read(`$python -m pip list`, String))
+    skater = mktempdir()
+    clone("https://github.com/oracle/Skater.git", skater)
+    run(`sed -i "s|sudo python|$python|g" $skater/setup.sh`)
+    run(`sed -i "s|.*install python.*||g" $skater/setup.sh`)
+    run(`sed -i "s|apt-get install|apt-get install -y|g" $skater/setup.sh`)
+    run(`bash -c "cd $skater && $python setup.py install --ostype=linux-ubuntu --rl=True"`)
+end
