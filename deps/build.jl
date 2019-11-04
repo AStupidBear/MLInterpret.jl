c524cfeaf9f7502c6192de6ffc4ef1b96327c755
Yao Lu <luyaocns@gmail.com>
2019-11-04 14:42:44 +0800

    run bindeps

--- deps/build.jl
+++ deps/build.jl
@@ -1,13 +1,17 @@
+using BinDeps, PyCall
 using PyCall: python
-using BinDeps
+using BinDeps: generate_steps, getallproviders, lower, PackageManager
 
 @BinDeps.setup
-gcc = library_dependency("gcc", runtime = false)
-graphviz = library_dependency("graphviz", runtime = false)
+gcc = library_dependency("gcc")
+dot = library_dependency("dot")
 if Sys.islinux()
-    provides(AptGet, Dict("gcc" => gcc, "graphviz" => graphviz))
-    provides(Yum, Dict("gcc-c++" => gcc, "graphviz" => graphviz))
+    provides(AptGet, Dict("gcc" => gcc, "graphviz" => dot))
+    provides(Yum, Dict("gcc-c++" => gcc, "graphviz" => dot))
 end
-@BinDeps.install
+for dep in bindeps_context.deps
+    dp, opts = getallproviders(dep, PackageManager)[1]
+    run(lower(generate_steps(dep, dp, opts)))
+end
 
 run(`$python -m pip install --user pandas sklearn matplotlib lightgbm shap keras tzlocal PyPDF2 unidecode skater`)
